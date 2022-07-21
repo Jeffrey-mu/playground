5c7b89475faf665ef42cb2c0c8e4d1330f38aaef
Jeffrey <1115378579@qq.com>
2022-07-21 23:24:42 +0800

    feat: driver demo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,28 +6,40 @@ const toggleLocales = () => {
   const locales = availableLocales
   locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
 }
+
+const packages = ref((useRouter().getRoutes()).filter(router => router.path.startsWith('/package')))
 </script>
 
 <template>
-  <nav text-xl mb-6>
-    <RouterLink text-left class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
-    </RouterLink>
+  <nav flex="~ justify-between" mb-6 bg-black px-5 py-3 c-white>
+    <div c-green>
+      playground
+    </div>
+    <div flex-1>
+      <RouterLink v-for="router, index in packages" :key="index" :to="router.path" hover="c-yellow">
+        {{ router.meta.name }}
+      </RouterLink>
+    </div>
+    <div>
+      <RouterLink text-left class="icon-btn mx-2" to="/" :title="t('button.home')">
+        <div i-carbon-campsite />
+      </RouterLink>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <div i="carbon-sun dark:carbon-moon" />
-    </button>
+      <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
-      <div i-carbon-language />
-    </a>
+      <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
+        <div i-carbon-language />
+      </a>
 
-    <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <div i-carbon-dicom-overlay />
-    </RouterLink>
+      <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+        <div i-carbon-dicom-overlay />
+      </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/Jeffrey-mu/vitesse" target="_blank" title="GitHub">
-      <div i-carbon-logo-github />
-    </a>
+      <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/Jeffrey-mu/vitesse" target="_blank" title="GitHub">
+        <div i-carbon-logo-github />
+      </a>
+    </div>
   </nav>
 </template>
---
+++ src/pages/package/driver.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import Driver from 'driver.js'
+import 'driver.js/dist/driver.min.css'
+const driver = new Driver()
+
+onMounted(() => {
+  driver.defineSteps([
+    {
+      element: '#Driver1',
+      popover: {
+        className: 'Driver1-step-popover-class',
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'left',
+      },
+    },
+    {
+      element: '#Driver2',
+      popover: {
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'top',
+      },
+    },
+    {
+      element: '#Driver3',
+      popover: {
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'right',
+      },
+    },
+    {
+      element: '#Driver4',
+      popover: {
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'right',
+      },
+    },
+    {
+      element: '#Driver5',
+      popover: {
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'right',
+      },
+    },
+  ])
+  driver.start()
+})
+const start = driver.start
+const IDS = ['Driver1', 'Driver2', 'Driver3', 'Driver4', 'Driver5']
+</script>
+
+<template>
+  <a href="/package/driver">
+    Reset
+  </a>
+
+  <div id="some-element" p-x50 flex="~ ">
+    <div v-for="id in IDS" :id="id" :key="id" ma w-20 h-20 border />
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+  name: 指引
+</route>
+
