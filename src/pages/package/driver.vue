3485efdceb4d27cc2d30ddfba325fd8e60f24ff2
Jeffrey <1115378579@qq.com>
2022-08-05 01:07:52 +0800

    fix: 删除指引

--- src/pages/package/driver.vue
+++ src/pages/package/driver.vue
@@ -1,59 +1,59 @@
 <script setup lang="ts">
-import Driver from 'driver.js'
+// import Driver from 'driver.js'
 import 'driver.js/dist/driver.min.css'
-const driver = new Driver()
+// const driver = new Driver()
 
-const start = () => {
-  driver.defineSteps([
-    {
-      element: '#Driver1',
-      popover: {
-        className: 'Driver1-step-popover-class',
-        title: 'Title on Popover',
-        description: 'Body of the popover',
-        position: 'left',
-      },
-    },
-    {
-      element: '#Driver2',
-      popover: {
-        title: 'Title on Popover',
-        description: 'Body of the popover',
-        position: 'left',
-      },
-    },
-    {
-      element: '#Driver3',
-      popover: {
-        title: 'Title on Popover',
-        description: 'Body of the popover',
-        position: 'right',
-      },
-    },
-    {
-      element: '#Driver4',
-      popover: {
-        title: 'Title on Popover',
-        description: 'Body of the popover',
-        position: 'right',
-      },
-    },
-    {
-      element: '#Driver5',
-      popover: {
-        title: 'Title on Popover',
-        description: 'Body of the popover',
-        position: 'left',
-      },
-    },
-  ])
-  driver.start()
-}
+// const start = () => {
+//   driver.defineSteps([
+//     {
+//       element: '#Driver1',
+//       popover: {
+//         className: 'Driver1-step-popover-class',
+//         title: 'Title on Popover',
+//         description: 'Body of the popover',
+//         position: 'left',
+//       },
+//     },
+//     {
+//       element: '#Driver2',
+//       popover: {
+//         title: 'Title on Popover',
+//         description: 'Body of the popover',
+//         position: 'left',
+//       },
+//     },
+//     {
+//       element: '#Driver3',
+//       popover: {
+//         title: 'Title on Popover',
+//         description: 'Body of the popover',
+//         position: 'right',
+//       },
+//     },
+//     {
+//       element: '#Driver4',
+//       popover: {
+//         title: 'Title on Popover',
+//         description: 'Body of the popover',
+//         position: 'right',
+//       },
+//     },
+//     {
+//       element: '#Driver5',
+//       popover: {
+//         title: 'Title on Popover',
+//         description: 'Body of the popover',
+//         position: 'left',
+//       },
+//     },
+//   ])
+//   driver.start()
+// }
 const IDS = ['Driver1', 'Driver2', 'Driver3', 'Driver4', 'Driver5']
 </script>
 
 <template>
-  <button @click.stop="start" border px-2 ma-2>
+  <button border px-2 ma-2 @click.stop="">
     start
   </button>
 
