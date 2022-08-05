125d9493cbb0921e47237203f8b1c2bbddd1e0db
Jeffrey <1115378579@qq.com>
2022-08-05 10:47:18 +0800

    chore: 优化home页面

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="text-center text-gray-700 dark:text-gray-200">
-    <Header />
-    <RouterView />
+  <main class="text-gray-700 dark:text-gray-200 overflow-hidden">
+    <Header position-fixed w100vw />
+    <RouterView mt-15 />
   </main>
 </template>
