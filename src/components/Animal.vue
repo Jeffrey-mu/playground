ad4e8904049c8602705a453a117c3d6dfb6d47ce
Jeffrey <1115378579@qq.com>
2022-08-05 10:46:10 +0800

    style: 优化随机狗样式

--- src/components/Animal.vue
+++ src/components/Animal.vue
@@ -8,7 +8,7 @@ const { option } = defineProps<AnimalProps>()
 const emit = defineEmits(['reload'])
 const path = computed(() => option.path.value)
 const showReload = computed(() => !option.showReload)
-const { isLoading, error } = useImage(path, { delay: 2000 })
+const { isLoading, error } = useImage(path, { delay: 3000 })
 function handleClick() {
   emit('reload')
 }
@@ -19,9 +19,9 @@ function handleClick() {
     <h2 fs-20 p-2 fw-600>
       {{ option.title }}
     </h2>
-    <div v-if="isLoading" b-rd w100 h90 class="animate-pulse bg-gray-500 p-2" />
+    <div v-if="isLoading" b-rd w100 h90 class="animate-pulse bg-gray-500/30 p-2" />
     <img v-else :src="path" alt="" ma b-rd w100 h90>
-    <button v-if="showReload" :disabled="disabled" b px-2 mt-2 b-rd hover="bg-black c-#fff cursor-pointer" @click="handleClick">
+    <button v-if="showReload" :disabled="disabled" btn m="3 t6" text-sm hover="bg-black c-#fff cursor-pointer" @click="handleClick">
       reload
     </button>
   </div>
