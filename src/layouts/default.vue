10b5235faee7a30f72ad9d8e60352dff845dc3f1
Jeffrey <1115378579@qq.com>
2022-08-05 10:47:02 +0800

    chore: 优化默认页面

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
-  <main class="text-center text-gray-700 dark:text-gray-200">
-    <Header />
-    <RouterView />
-    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm" />
+  <main class="text-gray-700 dark:text-gray-200">
+    <Header position-fixed />
+    <RouterView mt-15 />
   </main>
 </template>
