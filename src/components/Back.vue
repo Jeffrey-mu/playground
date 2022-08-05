7933c04073b1c5c032a7e08a50ab74849ba55dd0
Jeffrey <1115378579@qq.com>
2022-08-05 10:48:15 +0800

    feat: 添加back组件

---
+++ src/components/Back.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const router = useRouter()
+</script>
+
+<template>
+  <button
+    btn m="3 t6" text-sm
+    @click="router.back()"
+  >
+    {{ t('button.back') }}
+  </button>
+</template>
